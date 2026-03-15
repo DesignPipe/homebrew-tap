9d3110690e8767cc1525564993ae8ec20cb4a643
alexey1312 <alexey1312ru@gmail.com>
2026-03-15 20:11:20 +0500

    feat: add exfig formula (migrated from alexey1312/homebrew-tap)

---
+++ Formula/exfig.rb
@@ -0,0 +1,41 @@
+class Exfig < Formula
+  desc "Export colors, typography, icons, and images from Figma to Xcode, Android, Flutter, and Web"
+  homepage "https://github.com/DesignPipe/ExFig"
+  version "2.8.0"
+  license "MIT"
+
+  on_macos do
+    url "https://github.com/alexey1312/ExFig/releases/download/v#{version}/exfig-macos.zip"
+    sha256 "a2f39ee11e21c99c31a18d9ebbd7c1683ce4e320f2840952963a6aa277522382"
+
+    depends_on macos: :monterey
+  end
+
+  on_linux do
+    url "https://github.com/alexey1312/ExFig/releases/download/v#{version}/exfig-linux-x64.tar.gz"
+    sha256 "0fec8e76a436da6a8c8a654b5d4e1782d50d69b146af4fd98a21ad11f405e13c"
+  end
+
+  def install
+    # Binary and bundles must be co-located for Bundle.module to find templates
+    libexec.install "ExFig" => "exfig"
+    libexec.install Dir["exfig_*.bundle"]     # macOS resource bundles
+    libexec.install Dir["exfig_*.resources"]  # Linux resource bundles
+
+    # Symlink to bin for PATH access
+    bin.install_symlink libexec/"exfig"
+  end
+
+  def caveats
+    <<~EOS
+      Set your Figma Personal Access Token:
+        export FIGMA_PERSONAL_TOKEN=your_token_here
+
+      Get your token from: https://www.figma.com/developers/api#access-tokens
+    EOS
+  end
+
+  test do
+    assert_match "exfig", shell_output("#{bin}/exfig --version")
+  end
+end
